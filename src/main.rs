eba03b361b2847a56caab11aeaf60f29171415a1
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-05-17 11:43:54 -0400

    Add test framework intro

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 // main.rs
-#![no_std]      // Stops linking to Rust standard library
-#![no_main]     // Disables all rust-level entry points
+
+// Stops linking to Rust standard library
+#![no_std]
+// Disables all rust-level entry points
+#![no_main]     
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+// Set name of test framework function to test_main
+#![reexport_test_harness_main = "test_main"] 
 
 use core::panic::PanicInfo;
 
@@ -8,6 +15,10 @@ use core::panic::PanicInfo;
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -20,3 +31,21 @@ fn panic(info: &PanicInfo) -> ! {
 
 // Rust module to handle printing
 mod vga_buffer;
+
+// Custom test framework 
+#[cfg(test)]
+// Slice of trait object (&[&dyn Fn()]) that references the Fn() trait
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());     
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
