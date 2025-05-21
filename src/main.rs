d6546f79f355d882787065ef3eb1eb31ab6baa13
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-05-21 03:00:49 -0400

    Add QEMU exit function

--- src/main.rs
+++ src/main.rs
@@ -32,14 +32,35 @@ fn panic(info: &PanicInfo) -> ! {
 // Rust module to handle printing
 mod vga_buffer;
 
+// QEMU exit function with specified exit status 
+// (different from default QEMU codes)
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+// Create and write to new port
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 // Custom test framework 
 #[cfg(test)]
-// Slice of trait object (&[&dyn Fn()]) that references the Fn() trait
+// Slice of trait object (&[&dyn Fn()]) that 
+// references the Fn() trait
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());     
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
