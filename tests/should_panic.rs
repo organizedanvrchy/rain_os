85a2a4acee6ba8657475ed7ba9992fd8d1765d5f
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-05-23 22:30:39 -0400

    Update should_panic test

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,28 +1,21 @@
 // should_panic.rs
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use rain_os::{QemuExitCode, exit_qemu, serial_println};
+use rain_os::{QemuExitCode, exit_qemu, serial_println, serial_print};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    test_main();
-
+    should_fail();
+    serial_println!("[test did not panic");
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
+fn should_fail() {
+    serial_print!("should panic::shouldfail...\t");
+    assert_eq!(0, 1);
 }
 
 #[panic_handler]
@@ -32,10 +25,3 @@ fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
-use rain_os::serial_print;
-
-#[test_case]
-fn should_fail() {
-    serial_print!("should panic::shouldfail...\t");
-    assert_eq!(0, 1);
-}
