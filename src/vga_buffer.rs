0f7474ac004d94ce774e82d3d14b1859a5bf1b92
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-05-30 16:37:35 -0400

    Fix Deadlock

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -161,10 +161,16 @@ macro_rules! println {
     ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
 }
 
+// Print given formatted string to the VGA text buffer
+// through global `WRITER` instance,
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
-    WRITER.lock().write_fmt(args).unwrap();
+    use x86_64::instructions::interrupts;
+
+    interrupts::without_interrupts(|| {
+        WRITER.lock().write_fmt(args).unwrap();
+    });
 }
 
 // **Testing VGA Buffer**
