1f95e5fc842a042582c3d9270700dd502b5d62c4
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-06-06 21:43:25 -0400

    Comment Test from main

--- src/main.rs
+++ src/main.rs
@@ -39,10 +39,10 @@ pub extern "C" fn _start() -> ! {
     // println!("write worked!");
     
     // Accessing Page Tables
-    use x86_64::registers::control::Cr3;
+    // use x86_64::registers::control::Cr3;
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    // let (level_4_page_table, _) = Cr3::read();
+    // println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
 
     #[cfg(test)]
     test_main();
